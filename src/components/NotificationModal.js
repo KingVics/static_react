125c144dedd51b844aeaf08c2acf75a1bf8f4b51
Erioluwa Ogedengbe <erioluwa@ibcscorp.com>
2023-02-07 22:32:11 +0100

    yet to correct carousel

--- src/components/NotificationModal.js
+++ src/components/NotificationModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ModalIndex from "./Modal/ModalIndex";
 
 const NotificationModal = ({
@@ -6,19 +6,29 @@ const NotificationModal = ({
   setNotificationModal,
   submissionError,
 }) => {
+  const [mQuery, setMQuery] = useState({
+    matches: window.innerWidth > 768 ? true : false,
+  });
+
+  useEffect(() => {
+    let mediaQuery = window.matchMedia("(min-width: 768px)");
+    mediaQuery.addEventListener("resize", setMQuery, true);
+    return () => mediaQuery.removeEventListener("resize", setMQuery, true);
+  }, [mQuery]);
+
   return (
     <ModalIndex
       show={notificationModal}
       setShow={setNotificationModal}
       top={"25vh"}
-      left={"37%"}
+      left={mQuery ? "47% !important" : "37%"}
       background="#fff"
       initial={{ top: "0vh", left: "50%", opacity: 0 }}
       exit={{ top: "0vh", left: "50%", opacity: 0 }}
       animate={{ top: "50vh", left: "50%", opacity: 1 }}
       theme="rgba(0,0,0,.5)" // width={Query ? '50%' : '90%'}
       overFlow="auto"
-      width={"40%"}
+      width={mQuery ? "80%" : "40%"}
       height={"30%"}
       borderRadius={"3px"}
       ButtonBG={"rgb(240,240,240)"}
